2c900d775b2834a0487a1fa047f02e112bd9268a
Aamir Ashraf <aamirnasa97@gmail.com>
2023-02-01 21:13:45 +0530

    decorator is also works fine

--- app/signals.py
+++ app/signals.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.signals import user_logged_in,user_logged_out,user_login_failed
 from django.contrib.auth.models import User
+from django.dispatch import receiver
 
 #create the function for the successful login
+@receiver(user_logged_in,sender=User)
 def login_successful(sender,request,user,**kwargs):
     #sender and **kwargs is mandatory parameter
     print('log in signal .......')
@@ -10,5 +12,6 @@ def login_successful(sender,request,user,**kwargs):
     print(f'kwargs:{kwargs}')
 
 #now use .connect method manually
-user_logged_in.connect(login_successful,sender=User)
+# user_logged_in.connect(login_successful,sender=User)
+#we may use decorator instead fo the connect
 #we have pass login successful as the callback function here
